3bd293004d7f8f60a188af82f07dceaaff34664a
HaiNguyenPV12 <39506270+HaiNguyenPV12@users.noreply.github.com>
2020-01-07 13:35:11 +0700

    set up thymeleaf

---
+++ HTDShop/HTDShop/HTDShop-web/src/main/java/vn/htdshop/controller/shop/shopIndexController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.htdshop.controller.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Hai
+ */
+@Controller
+@RequestMapping("")
+public class shopIndexController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String getHome() {
+        return "index";
+    }
+}
