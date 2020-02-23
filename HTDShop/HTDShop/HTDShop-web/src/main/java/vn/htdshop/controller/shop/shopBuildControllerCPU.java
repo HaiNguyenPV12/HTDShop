c7d597cab6f148073c0799871bccd22cc2bd7161
Hai Nguyen <39506270+HaiNguyenPV12@users.noreply.github.com>
2020-02-23 21:07:30 +0700

    Update shopBuildControllerCPU.java

--- HTDShop/HTDShop/HTDShop-web/src/main/java/vn/htdshop/controller/shop/shopBuildControllerCPU.java
+++ HTDShop/HTDShop/HTDShop-web/src/main/java/vn/htdshop/controller/shop/shopBuildControllerCPU.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.servlet.http.HttpSession;
 
