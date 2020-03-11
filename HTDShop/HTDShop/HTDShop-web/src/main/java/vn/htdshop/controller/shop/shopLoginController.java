d0ae04595eb6b9f8724fb815c3a0a2c6e4d5ce2c
DungVD <dungvd0893@gmail.com>
2020-03-11 09:13:00 +0700

    Dung_change

--- HTDShop/HTDShop/HTDShop-web/src/main/java/vn/htdshop/controller/shop/shopLoginController.java
+++ HTDShop/HTDShop/HTDShop-web/src/main/java/vn/htdshop/controller/shop/shopLoginController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -71,6 +72,7 @@ public class shopLoginController {
                 return "redirect:/cart/checkout";
             }
         }
+        
         return "redirect:/login";
     }
 }
