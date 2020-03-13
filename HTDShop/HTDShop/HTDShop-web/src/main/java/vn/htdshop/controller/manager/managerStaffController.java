48b5ca9b4c75b7ebc271956c313706aa8275041c
Hai Thien <39566043+haithienht@users.noreply.github.com>
2020-03-13 17:30:27 +0700

    fix staff birthday format + triming inputs (except file and password)

--- HTDShop/HTDShop/HTDShop-web/src/main/java/vn/htdshop/controller/manager/managerStaffController.java
+++ HTDShop/HTDShop/HTDShop-web/src/main/java/vn/htdshop/controller/manager/managerStaffController.java
@@ -5,7 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
